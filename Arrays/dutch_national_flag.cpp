f2ea55edd11eeae1f38b4a1d78c2ff17f8998fce
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-03 16:14:35 +0530

    add: added solution for 012 aggregation for DNF

---
+++ Arrays/dutch_national_flag.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void DNF(int *arr, int n){
+  int c0=0, c1=0, c2=0, counter = 0;
+  for(int i = 0; i < n; i++){
+    switch(arr[i]){
+      case 0:
+        c0++;
+        break;
+      case 1:
+        c1++;
+        break;
+      case 2:
+        c2++;
+        break;
+    }
+  }
+
+  while(c0 > 0) arr[counter++] = 0, c0--;
+  while(c1 > 0) arr[counter++] = 1, c1--;
+  while(c2 > 0) arr[counter++] = 2, c2--;
+
+  cout << "Sorted array" << endl;
+
+  for(int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+}
+
+int main(){
+  int n, temp = 0;
+  cout << "Enter the size of the array: ";
+  cin >> n;
+  cout << endl;
+
+  int arr[n];
+  while(temp < n){
+    int value;
+    cout << "Enter the value: ";
+    cin >> value;
+    cout << endl;
+    if(value > 2) cout << "Please enter a value between 0 and 2" << endl;
+    else{
+      arr[temp] = value;
+      temp++;
+    }
+
+  }
+
+  DNF(arr, n);
+
+  return 0;
+}
