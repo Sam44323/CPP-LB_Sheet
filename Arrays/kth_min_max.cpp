12f0c03c31a9cbe4adbc5fcfa79f07d55d5dc4ee
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-03 15:56:14 +0530

    add: added support for kth-min-max value

--- Arrays/kth_min_max.cpp
+++ Arrays/kth_min_max.cpp
@@ -2,3 +2,34 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::sort;
+
+void kth_min_max(int *arr, int length, int k){ 
+  if(k > length){
+    cout << "Invalid kth value" << endl;
+    return;
+  }
+  sort(arr, arr + length);
+  cout << "The " << k << "-> smallest element is " << arr[k-1] << endl;
+  cout << "The " << k << "-> largest element is " << arr[length-k] << endl;
+}
+
+int main(){
+  int n, k;
+  cout << "Enter the length for the array: ";
+  cin >> n;
+  cout << endl;
+
+  int arr[n];
+
+  cout << "Enter the elements for the array" << endl;
+  for(int i = 0; i < n; i++) cin >> arr[i];
+
+  cout << "Enter the value of k: ";
+  cin >> k;
+  cout << endl;
+
+  kth_min_max(arr, n, k);
+
+  return 0;
+}
