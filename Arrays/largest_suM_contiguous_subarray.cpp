008fd077f8c567821c10766cd84be3060c495913
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-07 23:12:59 +0530

    add: added initiater for largest_sum contiguous-array

---
+++ Arrays/largest_suM_contiguous_subarray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::endl;
+using std::cin;
+using std::cout;
+
+void Largest_Sum_Contiguous_Subarray(int *arr, int length){}
+
+int main(){
+  int n;
+  cout << "Enter the lenght of the array: ";
+  cin >> n;
+
+  int arr[n];
+
+  cout << "Enter the elements of the array: " << endl;
+  for(int i = 0; i < n; i++)cin >> arr[i];
+  return 0;
+}
