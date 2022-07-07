9683d9b3d573769eb5683883122a149d8ab7677c
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-07 23:25:25 +0530

    add: added code for largest contiguous-sum

--- Arrays/largest-sum_contiguous_subarray.cpp
+++ Arrays/largest-sum_contiguous_subarray.cpp
@@ -3,7 +3,16 @@ using std::endl;
 using std::cin;
 using std::cout;
 
-void Largest_Sum_Contiguous_Subarray(int *arr, int length){}
+void Largest_Sum_Contiguous_Subarray(int *arr, int length){
+  int max_sum = 0;
+  int curr_sum = 0;
+  for(int i = 0; i < length; i++){
+    curr_sum += arr[i];
+    if(curr_sum > max_sum) max_sum = curr_sum;
+    if(curr_sum < 0) curr_sum = 0;
+  }
+  cout << "The largest sum of contiguous subarray is: " << max_sum << endl;
+}
 
 int main(){
   int n;
@@ -14,5 +23,7 @@ int main(){
 
   cout << "Enter the elements of the array: " << endl;
   for(int i = 0; i < n; i++)cin >> arr[i];
+
+  Largest_Sum_Contiguous_Subarray(arr, n);
   return 0;
 }
