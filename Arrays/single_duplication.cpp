549eb460420d263605b35dbf0e01c2b367f08b17
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-09 00:46:39 +0530

    add: added code for duplicated singular

---
+++ Arrays/single_duplication.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+using std::cin;
+using std::sort;
+
+void duplication_count(int *arr, int length){
+  int count = 0;
+  sort(arr, arr + length);
+
+  for(int i = 0; i < length - 1; i++){
+    if(arr[i] == arr[i + 1]) count++;
+  }
+
+  cout << "The number of duplications is " << count << endl;
+}
+
+int main(){
+  int n;
+  cout << "Enter the length of the array: ";
+  cin >> n;
+  cout << endl;
+
+  int arr[n];
+  cout << "Enter the array: " << endl;
+  for(int i = 0; i < n; i++) cin >> arr[i];
+
+  duplication_count(arr, n);
+  return 0;
+}
