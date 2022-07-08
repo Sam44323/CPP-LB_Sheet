5be3e0eaebefa93a2f6b33c723543e27920d0b77
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-09 00:08:55 +0530

    add: added declaration for max-jumper

--- Arrays/max_jumps.cpp
+++ Arrays/max_jumps.cpp
@@ -3,6 +3,17 @@ using std::endl;
 using std::cin;
 using std::cout;
 
+int max_jump(int *arr, int length){}
+
 int main(){
+  int n;
+  cout << "Enter the length of the array: ";
+  cin >> n;
+
+  int arr[n];
+  cout << "Enter the array: " << endl;
+  for(int i = 0; i < n; i++) cin >> arr[i];
+
+  cout << "The maximum number of jumps is " << max_jump(arr, n) << endl;
   return 0;
 }
