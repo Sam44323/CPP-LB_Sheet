1ea3e855946b276e64b3775ada25ba90b7e70efc
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-15 23:29:03 +0530

    y

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -1,6 +1,23 @@
 #include "../common_imports.h"
 
-void bubbleSort(int arr[], int len){}
+void bubbleSort(int arr[], int len){
+  int i = 0,j= 0, temp = 0;
+
+  for(i = 0; i < len; i++){
+    for(j = 0; j < len - 1; j++){
+      if(arr[j] > arr[j+1]){
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+
+  cout << "Array in ascending order: " << endl;
+  for(i = 0; i < len; i++){
+    cout << arr[i] << " ";
+  }
+}
 
 int main(){
   int n;
