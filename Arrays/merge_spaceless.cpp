f131f2c88b01d69b97e1da92e8e43d0d85a29a85
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-15 15:32:52 +0530

    add: added code for array-declaration

--- Arrays/merge_spaceless.cpp
+++ Arrays/merge_spaceless.cpp
@@ -3,6 +3,20 @@
 void merge_spaceless(int arr1[], int arr2[], int l1, int l2){}
 
 int main(){
-  int n;
+  int n1 , n2, i=0;
+  cout << "Enter the length of the first-array! " << endl;
+  cin >> n1;
+  int arr1[n1];
+  for(i = 0; i < n1; i++)
+    cin >> arr1[i];
+
+  cout << "Enter the length of the second-array! " << endl;
+  cin >> n2;
+  int arr2[n2];
+
+  for(i = 0; i < n2; i++)
+    cin >> arr2[i];
+
+  merge_spaceless(arr1, arr2, n1, n2);
   return 0;
 }
